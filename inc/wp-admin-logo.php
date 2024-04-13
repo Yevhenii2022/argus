62e6548e3546e8193f32241cb5c5c1ad4c444478
Yevhenii2022 <3starosta3@gmail.com>
2024-04-14 01:07:15 +0300

    fix init style

--- inc/wp-admin-logo.php
+++ inc/wp-admin-logo.php
@@ -2,13 +2,18 @@
 // Add actually link in background-image: url
 { ?>
 
+    <?php
+    $upload_dir = wp_get_upload_dir();
+    $attachment_url = $upload_dir['baseurl'] . '/2024/03/logo.svg';
+    ?>
+
     <style type="text/css">
         #login h1 a {
-            background-image: url('/wp-content/themes/pointer-theme/src/img/logo.svg');
+            background-image: url(<?php echo esc_url($attachment_url); ?>);
             display: block;
             width: 272px;
             height: 92px;
-            background-size: contain;
+            background-size: auto;
         }
     </style>
 <?php }
