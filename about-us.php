33be93f7fc2e1cb04002c4bbf75062ca1cad3fc9
litvinenkoiryna <litvinenkoiryna@gmail.com>
2024-04-15 09:50:25 +0300

    mission

--- about-us.php
+++ about-us.php
@@ -71,8 +71,94 @@ get_header();
                 </div>
             </div>
     </section>
+
+    <?php $stageBackground = get_field('stage_bg') ?? '' ;
+          $stageTitle = get_field('stage_heading') ?? '' ;
+          $stageSubtitle = get_field('stage_subtitle') ?? '' ;?>
     <section class="about-stages">
+        <!-- <div class="container"> -->
+            <div class="about-stages__wrapper">
+                <?php if( $stageBackground || $stageTitle|| $stageSubtitle): ?>
+                <div class="about-stages__heading">
+                    <?php if ($stageTitle): ?>
+                    <h2 class="about-stages__section-title main__title main__title--sm">
+                        <?= $stageTitle ;?>
+                    </h2>
+                    <?php endif ?>
+                    <?php if ($stageSubtitle): ?>
+                    <p class="about-stages__subtitle">
+                        <svg viewBox="0 0 9 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4.24528 11H0L4.75472 5.5L0 0H4.24528L9 5.5L4.24528 11Z" fill="#F41B1B" />
+                        </svg>
+                        <?= $stageSubtitle ;?>
+                    </p>
+                    <?php endif ?>
+                </div>
+                <?php endif ?>
+
+                <div class="about-stage__item">
+                    <?php while (have_rows('stages')):
+                            the_row();
+                            $startDate = get_sub_field('start_date');
+                            $endDate = get_sub_field('end_date');
+                            $stageHeading= get_sub_field('stage_title'); 
+                            $stageDescription = get_sub_field('stage_description');
+                            $stageImg = get_sub_field('stage_img'); ?>
+                    <div class="about-stages__item-inner">
+                        <div
+                            class="about-stages__bg"
+                            style="background-image: url('<?php echo esc_url($stageBackground); ?>')">
+                        </div>
+                        <?php if ($startDate): ?>
+                        <div class="about-stages__year-first">
+                            <?= $startDate ;?>
+                        </div>
+                        <?php endif ?>
+                        <?php if ($endDate): ?>
+                        <div class="about-stages__year-last">
+                            <?= $endDate ;?>
+                        </div>
+                        <?php endif ?>
+
+                        <div class="about-stages__text">
+                            <?php if ($stageHeading): ?>
+                            <div class="about-stages__title">
+                                <?= $stageHeading ;?>
+                            </div>
+                            <?php endif ?>
+                            <?php if ($stageDescription): ?>
+                            <div class="about-stages__description">
+                               <?= $stageDescription ;?>
+                            </div>
+                            <?php endif ?>
+                        </div>
+                        <?php if ($stageImg): ?>
+                        <div class="about-stages__img" data-image-wrap>
+                            <img src='<?php echo $stageImg['url']; ?>' alt='<?php echo $stageImg['alt']; ?>' />
+                        </div>
+                        <?php endif ?>
+                    </div>
+                    <? endwhile ;?>
+                </div>
+                
+            </div>
+        <!-- </div> -->
         
+    </section>
+
+    <?php $missionBackground = get_field('mission_bg') ?? '' ;?>
+    <section class="about-mission" style=" background-image: url('<?php echo esc_url($missionBackground); ?>');">
+        <div class="container">
+            <div class="about-mission__wrapper">
+                <h2 class="about-mission__title"></h2>
+                <div class="about-mission__text">
+                    <p class="about-mission__description"></p>
+                    <p class="about-mission__paragraph"></p>
+                    <div class="about-mission__icon"></div>
+                </div>
+            </div>
+        </div>
+        <div class="about-mission__image"></div>
     </section>
     
 </main>
