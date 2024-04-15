9ca56b1553de317b0a6f07d6905fc916403ec41b
Yevhenii2022 <3starosta3@gmail.com>
2024-04-15 21:52:39 +0300

    add about

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -1,3 +1,5 @@
+<?php get_template_part('inc/post-type-projects'); ?>
+
 <?php
 // SERVICES POST TYPE
 function create_services_posttype()
@@ -20,50 +22,3 @@ function create_services_posttype()
     );
 }
 add_action('init', 'create_services_posttype');
-
-
-// PROJECTS POST TYPE
-
-function create_projects_posttype()
-{
-
-    register_post_type(
-        'projects',
-        array(
-            'labels' => array(
-                'name' => ('Проєкти'),
-                'singular_name' => ('Проєкт')
-            ),
-            'public' => true,
-            'has_archive' => false,
-            'rewrite' => array('slug' => 'projects'),
-            'show_in_rest' => true,
-            'menu_icon' => 'dashicons-admin-multisite',
-            'supports' => array('title', 'except', 'thumbnail', 'editor', 'custom-fields')
-        )
-    );
-}
-add_action('init', 'create_projects_posttype');
-
-// PROJECTS POST TYPE
-
-function create_news_posttype()
-{
-
-    register_post_type(
-        'news',
-        array(
-            'labels' => array(
-                'name' => ('Новини'),
-                'singular_name' => ('Новина')
-            ),
-            'public' => true,
-            'has_archive' => false,
-            'rewrite' => array('slug' => 'news'),
-            'show_in_rest' => true,
-            'menu_icon' => 'dashicons-testimonial',
-            'supports' => array('title', 'except', 'thumbnail', 'editor', 'custom-fields')
-        )
-    );
-}
-add_action('init', 'create_news_posttype');
---
+++ inc/post-type-projects.php
@@ -0,0 +1,40 @@
+<?php
+
+function projects_custom_post()
+{
+  $labels = array(
+    'name' => esc_html__('Проєкти', 'pointer_theme'),
+    'singular_name' => esc_html__('Проєкт', 'pointer_theme'),
+    'add_new' => esc_html__('Додати новий проєкт', 'pointer_theme'),
+    'add_new_item' => esc_html__('Додати новий проєкт', 'pointer_theme'),
+    'edit_item' => esc_html__('Редагувати проєкт', 'pointer_theme'),
+    'new_item' => esc_html__('Новий проєкт', 'pointer_theme'),
+    'all_items' => esc_html__('Всі проєкти', 'pointer_theme'),
+    'view_item' => esc_html__('Переглянути проєкт', 'pointer_theme'),
+    'search_items' => esc_html__('Знайти проєкт', 'pointer_theme'),
+    'not_found' => esc_html__('Проєктів не знайдено', 'pointer_theme'),
+    'featured_image' => esc_html__('Зображення', 'pointer_theme'),
+    'set_featured_image' => esc_html__('Додати зображення', 'pointer_theme')
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'description' => 'Проєкти',
+    'public' => true,
+    'menu_position' => 19,
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'has_archive' => false,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'rewrite' => array('slug' => 'projects'),
+    'menu_icon' => 'dashicons-admin-multisite',
+    'publicly_queryable' => false,
+    'query_var' => true,
+    'taxonomies' => array('category')
+  );
+
+  register_post_type('projects', $args);
+}
+
+add_action('init', 'projects_custom_post');
