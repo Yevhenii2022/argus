b2fc052ae02eb529b52169603b77e6634be47260
Yevhenii2022 <3starosta3@gmail.com>
2024-04-17 10:53:52 +0300

    add projects page

--- projects.php
+++ projects.php
@@ -9,7 +9,52 @@ get_header(); ?>
     <section class="projects">
         <div class="container">
             <div class="projects__wrapper">
-                <p>text</p>
+
+                <div class="projects__top">
+
+                    <?php if (function_exists('yoast_breadcrumb')) {
+                        yoast_breadcrumb('<nav class="yoast-breadcrumbs">', '</nav>');
+                    } ?>
+
+                    <h1 class="projects__title title"><?php the_content(); ?></h1>
+
+                </div>
+
+
+
+
+
+
+                <?php
+                $args = array(
+                    'post_type' => 'projects',
+                    'posts_per_page' => 6,
+                );
+                $projects_query = new WP_Query($args);
+                if ($projects_query->have_posts()) :
+                ?>
+                    <div class="projects-part__list">
+                        <?php
+                        while ($projects_query->have_posts()) : $projects_query->the_post();
+                        ?>
+
+                            <?php get_template_part('template-parts/project-card'); ?>
+
+                        <?php endwhile;
+                        wp_reset_postdata();
+                        ?>
+                    </div>
+                <?php endif; ?>
+
+
+                <button type='button' class="projects__button button">
+                    <div class="button__wrapper">
+                        <p> <?php pll_e('download more'); ?></p>
+                    </div>
+                </button>
+
+
+
             </div>
         </div>
     </section>
