aa9922de54be631d8151fd0ff409699a1c3e1518
litvinenkoiryna <litvinenkoiryna@gmail.com>
2024-05-02 19:53:47 +0300

    404 & privacy policy

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * Template Name: Privacy Policy
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -16,7 +16,33 @@ get_header();
 ?>
 
 <main>
-	<h1>DEFAULT TEMPLATE</h1>
+	<?php $policyBg = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
+	      $mainInfo = get_field('main_info') ?? '' ?>
+	<section class="policy" >
+		<div class="container">
+			<?php if($policyBg) : ?>
+			<div class="policy__bg" style="background-image: url('<?php echo esc_url($policyBg); ?>');"></div>
+			<?php endif ?>
+			
+			<div class="policy__wrapper">
+				<div class="policy__content">
+						<h1 class="policy__title">
+							<?php	the_title(); ?>
+						</h1>
+						<?php if($policyBg) : ?>
+							<p class="policy__info">
+								<?= $mainInfo ;?>
+							</p>
+						<?php endif ?>
+						<div class="policy__text">
+							<?php the_content();	?>
+						</div>			
+				</div>
+			
+			</div>
+		</div>
+	</section>
+	<?php get_template_part('template-parts/contact-us'); ?>
 </main>
 
 <?php
