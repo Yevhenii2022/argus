b86bafc399913e99a0bb46b894429c1057e5cc31
Yevhenii2022 <3starosta3@gmail.com>
2024-05-15 12:55:33 +0300

    add svg animation

--- src/js/parts/svg.js
+++ src/js/parts/svg.js
@@ -27,3 +27,30 @@
 
 //   animateOnScroll();
 // });
+
+document.addEventListener('DOMContentLoaded', function () {
+	function animateOnScroll() {
+		var block = document.getElementById('bcg');
+		var el = document.getElementById('about-svg2');
+
+		if (isElementInViewport(block)) {
+			el.classList.add('animate');
+		}
+	}
+
+	function isElementInViewport(el) {
+		var top = el.offsetTop;
+		var height = el.offsetHeight;
+
+		while (el.offsetParent) {
+			el = el.offsetParent;
+			top += el.offsetTop;
+		}
+
+		return top < window.pageYOffset + window.innerHeight && top + height > window.pageYOffset;
+	}
+
+	window.addEventListener('scroll', animateOnScroll);
+
+	animateOnScroll();
+});
